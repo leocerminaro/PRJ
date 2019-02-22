b7c48c2193e04faced4be975fb6bf8c9209c904f
Inocentini <mateus.inocentini@gmail.com>
2019-02-21 21:47:42 -0300

    First commit

---
+++ app/src/main/java/com/ifsp/soswoman/GenerateLocation.kt
@@ -0,0 +1,7 @@
+package com.ifsp.soswoman
+
+import com.google.android.gms.maps.model.LatLng
+
+class GenerateLocation {
+    //val location1 = LatLng()
+}
---
+++ app/src/main/java/com/ifsp/soswoman/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ifsp.soswoman
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+
+            val location1 = LatLng(-23.54,-46.63)
+            googleMap.addMarker(MarkerOptions().position(location1).title("Minha Localizacao"))
+
+        })
+    }
+}
